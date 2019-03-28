c68ea071ef41319d9153e30106dab79b269b5991
simplizm-company <40747170+simplizm-company@users.noreply.github.com>
2019-03-28 13:42:40 +0900

    br remove

--- docs/kronos.js
+++ docs/kronos.js
@@ -12,11 +12,9 @@
 
     Kronos = (function () {
         var fnidx = 0;
-
         function kronos(element, settings){
             var _ = this;
             var settings = settings === undefined ? {} : settings;
-
             var defaults = {
                 initDate: null,
                 format: 'yyyy-mm-dd',
@@ -48,38 +46,28 @@
                     //console.log(date);
                 }
             }
-
             _.options = _.hasOwnProperty(defaults, settings);
-
             _.initial = {
                 fnidx: ++fnidx,
-
                 indexYS: null,
                 indexYE: null,
                 indexMS: null,
                 indexME: null,
                 indexDS: null,
                 indexDE: null,
-
                 formatY: null,
                 formatM: null,
                 formatD: null,
-
                 weekCount: 0,
-
                 actionFlag: false
             };
-
             _.strings = {}
-
             _.corevar = {
                 today: null
             }
-
             _.events = {
                 click: 'click.kronos'+_.initial.fnidx
             }
-
             _.markups = {
                 common: {
                     outer: `<div class="kronos-outer">`,
@@ -121,7 +109,6 @@
                     content: null
                 }
             }
-
             _.element = {
                 kronos: $(element).attr('readonly', 'readonly').addClass('kronos-input'),
                 kronosF: _.options.periodFrom ? $(_.options.periodFrom) : false,
@@ -131,10 +118,8 @@
                 month: {},
                 date: {}
             }
-
             _.init();
         }
-
         return kronos;
     }());
 
@@ -164,12 +149,10 @@
 
     Kronos.prototype.convertFormat = function(core) {
         var _ = this, f, y, m, d;
-
         y = _.initial.formatY.length === 2 ? String(core.substring(2, 4)) : String(core.substring(0, 4));
         m = String(core.substring(4, 6));
         d = String(core.substring(6, 8));
         f = _.options.format.replace(_.initial.formatY, y).replace(_.initial.formatM, m).replace(_.initial.formatD, d);
-
         return f;
     }
 
@@ -204,14 +187,12 @@
 
     Kronos.prototype.slideDatepicker = function (delta, target1, target2, callback) {
         var _ = this;
-
         _.initial.actionFlag = true;
         setTimeout(function () {
             if (target2) {
                 target2.css(_.autoPrefixer(-delta * 100))
             }
             target1.css(_.autoPrefixer(0));
-
             setTimeout(function () {
                 if (target2) {
                     target2.remove();
@@ -225,7 +206,6 @@
     Kronos.prototype.visibleMotion = function (type, show, hide) {
         var class1 = type === 'in' ? 'kronos-outer-scale-h' : 'kronos-outer-scale-r';
         var class2 = type === 'in' ? 'kronos-outer-scale-r' : 'kronos-outer-scale-h';
-
         hide.addClass(class1);
         hide.addClass('kronos-outer-hide');
         show.addClass('kronos-outer-show');
@@ -244,14 +224,12 @@
     // start: only one run function =========================================================================================================================
     Kronos.prototype.checkFormat = function () {
         var _ = this;
-
         _.initial.indexYS = _.options.format.indexOf('y');
         _.initial.indexYE = _.options.format.lastIndexOf('y')+1;
         _.initial.indexMS = _.options.format.indexOf('m');
         _.initial.indexME = _.options.format.lastIndexOf('m')+1;
         _.initial.indexDS = _.options.format.indexOf('d');
         _.initial.indexDE = _.options.format.lastIndexOf('d')+1;
-
         _.initial.formatY = String(_.options.format.substring(_.initial.indexYS, _.initial.indexYE));
         _.initial.formatM = String(_.options.format.substring(_.initial.indexMS, _.initial.indexME));
         _.initial.formatD = String(_.options.format.substring(_.initial.indexDS, _.initial.indexDE));
@@ -259,22 +237,18 @@
 
     Kronos.prototype.getTodayDate = function () {
         var _ = this;
-
         _.initial.date = new Date();
         _.initial.todayY = _.initial.date.getFullYear();
         _.initial.todayM = _.initial.date.getMonth();
         _.initial.todayD = _.initial.date.getDate();
         _.initial.dateLeng = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
         _.corevar.today = String(_.initial.todayY) + _.combineZero(_.initial.todayM + 1) + _.combineZero(_.initial.todayD);
     }
 
     Kronos.prototype.setLayout = function () {
         var _ = this;
-
         _.element.common.outer = _.element.kronos.wrap(_.markups.common.outer).parent('.kronos-outer');
         _.element.common.inner = _.element.common.outer.append(_.markups.common.inner).children('.kronos-inner');
-
         _.element.year.outer = _.element.common.inner.append(_.markups.year.outer).children('.kronos-year-outer');
         _.element.month.outer = _.element.common.inner.append(_.markups.month.outer).children('.kronos-month-outer');
         _.element.date.outer = _.element.common.inner.append(_.markups.date.outer).children('.kronos-date-outer');
@@ -282,7 +256,6 @@
 
     Kronos.prototype.setKronosValue = function () {
         var _ = this;
-
         if (_.options.initDate) {
             _.corevar.selectedC = _.options.initDate;
             _.corevar.selectedF = _.convertFormat(_.corevar.selectedC);
@@ -292,7 +265,6 @@
 
     Kronos.prototype.setVisible = function () {
         var _ = this;
-
         if (_.options.visible) {
             _.element.common.outer.addClass('kronos-visible');
             _.openDatepicker();
@@ -305,13 +277,10 @@
     Kronos.prototype.openDatepicker = function () {
         // 달력 열기
         var _ = this;
-
         _.setDateInit(0); // 데이트피커 하위 마크업
-
         _.element.common.outer.addClass('kronos-open');
         _.element.year.outer.addClass('kronos-outer-scale-h');
         _.element.month.outer.addClass('kronos-outer-scale-h');
-
         // onEvents
         _.eventsRegister.close(_);
     }
@@ -319,20 +288,16 @@
     Kronos.prototype.closeDatepicker = function () {
         // 달력 닫기
         var _ = this;
-
         _.element.year.outer.empty();
         _.element.month.outer.empty();
         _.element.date.outer.empty();
-
         _.element.common.outer.removeClass('kronos-open');
-
         // offEvents
         $(document).off(_.events.click);
     }
 
     Kronos.prototype.setThisDate = function () {
         var _ = this;
-
         _.initial.thisY = _.initial.date.getFullYear();
         _.initial.startY = _.initial.thisY - 4 < 0 ? 0 : _.initial.thisY - 4;
         _.initial.endY = _.initial.startY + 8;
@@ -355,11 +320,8 @@
     Kronos.prototype.setDateInit = function (delta) {
         // 달력 그리기
         var _ = this;
-
         _.initial.date.setMonth(_.initial.date.getMonth() + delta);
-
         _.setThisDate(); // 오늘 날짜 데이터 설정
-
         _.element.date.inner = _.element.date.outer.append(_.markups.date.inner).children('.kronos-date-inner:last-child');
         _.element.date.head = _.element.date.inner.append(_.markups.date.head).children('.kronos-date-head');
         _.element.date.body = _.element.date.inner.append(_.markups.date.body).children('.kronos-date-body');
@@ -368,9 +330,7 @@
         _.element.date.year = _.element.date.title.append('<span class="kronos-date-year">'+_.initial.thisY+_.options.text.thisYear+'</span>').children('.kronos-date-year');
         _.element.date.month = _.element.date.title.append('<span class="kronos-date-month">'+_.options.text.month[_.initial.thisM]+_.options.text.thisMonth+'</span>').children('.kronos-date-month');
         _.element.date.next = _.element.date.head.append(_.markups.date.next).children('.kronos-date-next');
-
         _.element.date.inner.css(_.autoPrefixer(delta * 100));
-
         _.setDayMarkup(); // 요일 마크업 그리기
         _.setDateMarkup(); // 각 날짜 마크업 그리기
         _.getCoreData(); // 코어 데이타 (this, from, to) 가져오기
@@ -379,37 +339,30 @@
             _.element.date.inner2 = _.element.date.inner;
             _.element.date.inner = null;
         });
-
         // onEvents
         _.eventsRegister.date(_);
     }
 
     Kronos.prototype.setMonthInit = function (delta) {
         var _ = this;
-
         _.element.month.inner = _.element.month.outer.append(_.markups.month.inner).children('.kronos-month-inner:last-child');
         _.element.month.head = _.element.month.inner.append(_.markups.month.head).children('.kronos-month-head');
         _.element.month.body = _.element.month.inner.append(_.markups.month.body).children('.kronos-month-body');
         _.element.month.prev = _.element.month.head.append(_.markups.month.prev).children('.kronos-month-prev');
         _.element.month.next = _.element.month.head.append(_.markups.month.next).children('.kronos-month-next');
-
         _.element.month.inner.css(_.autoPrefixer(delta * 100));
-
         _.slideDatepicker(delta, _.element.month.inner, _.element.month.inner2, function () {
             _.element.month.inner2 = _.element.month.inner;
             _.element.month.inner = null;
         });
         _.setMonthMarkup(delta);
-
         _.element.month.title = _.element.month.head.append(_.markups.month.title).children('.kronos-month-title').text(_.initial.thisY);
-
         // onEvents
         _.eventsRegister.month(_);
     }
 
     Kronos.prototype.setYearInit = function (delta) {
         var _ = this;
-
         if (!delta && _.element.year.inner2) {
             _.element.year.inner2.remove();
         }
@@ -418,27 +371,21 @@
         _.element.year.body = _.element.year.inner.append(_.markups.year.body).children('.kronos-year-body');
         _.element.year.prev = _.element.year.head.append(_.markups.year.prev).children('.kronos-year-prev');
         _.element.year.next = _.element.year.head.append(_.markups.year.next).children('.kronos-year-next');
-
         _.element.year.inner.css(_.autoPrefixer(delta * 100));
-
         _.initial.startY = _.initial.thisY - 4 < 0 ? 0 : _.initial.thisY - 4;
         _.initial.endY = _.initial.startY + 8;
-
         _.slideDatepicker(delta, _.element.year.inner, _.element.year.inner2, function () {
             _.element.year.inner2 = _.element.year.inner;
             _.element.year.inner = null;
         });
         _.setYearMarkup(delta);
-
         _.element.year.title = _.element.year.head.append(_.markups.year.title).children('.kronos-year-title').text(_.initial.startY+_.options.text.thisYear+' ~ '+_.initial.endY+_.options.text.thisYear);
-
         // onEvents
         _.eventsRegister.year(_);
     }
 
     Kronos.prototype.setDayMarkup = function () {
         var _ = this;
-
         _.markups.date.content = '<table><thead><tr>';
         for (var i = 0; i < 7; i++) {
             _.markups.date.content += '<th>'+_.options.text.days[i]+'</th>';
@@ -449,15 +396,12 @@
     Kronos.prototype.setDateMarkup = function () {
         var _ = this;
         _.initial.prevMonthFirstDate = _.initial.dateLeng[_.initial.prevM] - _.initial.thisDay;
-
         _.markups.date.content += '<tbody><tr>';
-
         for (var i = 1; i <= _.initial.thisDay; i++) {
             // 이전달 마크업
             _.markups.date.content += '<td><button type="button" core="'+_.strings.prevYM+(_.initial.prevMonthFirstDate+i)+'" title="'+_.convertFormat(_.strings.prevYM+(_.initial.prevMonthFirstDate+i))+'">'+(_.initial.prevMonthFirstDate+i)+'</button></td>';
             _.initial.weekCount++;
         }
-
         for (var i = 1; i <= _.initial.thisDateLeng; i++) {
             // 이번달 마크업
             if (_.initial.weekCount === 0) {
@@ -470,7 +414,6 @@
                 _.initial.weekCount = 0;
             }
         }
-
         for (var i = 1; _.initial.weekCount != 0; i++) {
             // 다음달 마크업
             if (_.initial.weekCount === 7) {
@@ -481,17 +424,14 @@
                 _.initial.weekCount++;
             }
         }
-
         _.markups.date.content += '</tbody></table>';
         _.element.date.body.html(_.markups.date.content);
         _.element.common.inner.css({'height': _.element.date.inner.outerHeight()});
-
         _.element.date.date = _.element.date.body.find('td');
     }
 
     Kronos.prototype.getCoreData = function () {
         var _ = this;
-
         if (_.element.kronos.attr('core')) {
             _.corevar.selectedC = _.element.kronos.attr('core');
             _.corevar.selectedF = _.convertFormat(_.corevar.selectedC);
@@ -510,7 +450,6 @@
 
     Kronos.prototype.setDateClasses = function () {
         var _ = this;
-
         _.element.date.date.removeClass();
         _.element.date.date.each(function () {
             this.index = $(this).index();
@@ -545,7 +484,6 @@
 
     Kronos.prototype.setMonthMarkup = function (delta) {
         var _ = this;
-
         _.initial.thisY = parseInt(_.initial.thisY) + parseInt(delta);
         for (var i = 0; i < 12; i++) {
             _.element.month.body.append('<button core="'+_.initial.thisY+_.combineZero(i+1)+'" class="'+(_.initial.thisY === _.initial.date.getFullYear() && i === _.initial.thisM ? 'selected' : '')+'">'+(i+1)+'</button>');
@@ -555,7 +493,6 @@
 
     Kronos.prototype.setYearMarkup = function (delta) {
         var _ = this;
-
         _.initial.startY = _.initial.startY + (9 * delta) < 0 ? 0 : _.initial.startY + (9 * delta);
         _.initial.thisY = _.initial.thisY + (9 * delta);
         _.initial.endY = _.initial.startY + 8;
@@ -667,19 +604,16 @@
 
     Kronos.prototype.init = function () {
         var _ = this;
-
         _.checkFormat();
         _.getTodayDate();
         _.setLayout();
         _.setKronosValue();
         _.setVisible();
-
         _.eventsRegister.open(_);
     }
 
     Kronos.prototype.resetPeriod = function () {
         var _ = this;
-
         _.element.kronos.val(null).attr('core', null);
         _.corevar.selectedC = null;
         _.corevar.selectedF = null;
@@ -687,7 +621,6 @@
         _.corevar.fromF = null;
         _.corevar.toC = null;
         _.corevar.toF = null;
-
         _.setDateClasses();
     }
 
@@ -697,7 +630,6 @@
             s = Array.prototype.slice.call(arguments, 1),
             l = _.length,
             r;
-
         for(var i = 0; i < l; i++) {
             if (typeof o == 'object' || typeof o == 'undefined') {
                 _[i].Kronos = new Kronos(_[i], o);
@@ -708,7 +640,6 @@
                 }
             }
         }
-
         return _;
     }
 }(jQuery));
